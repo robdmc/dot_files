cd73d9c12509e216967c4b2f3e520f759e74b53f
Rob deCarvalho <robdmc@gmail.com>
2012-05-07 18:40:06 +0000

    Added a python script for deploying setup files.

---
+++ deploy.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+
+import os
+
+#--- create a list of files that should just be directly linked to home
+linkFileList = [
+               '.bash_profile',
+               '.bashrc',
+               '.inputrc',
+               '.vim',
+               '.vimrc'
+               ]
+
+#--- link the files to home
+for linkFile in linkFileList:
+    cmd = "cd ~; ln -sf ./setupFiles/%s ." % linkFile
+    print cmd
+    os.system(cmd)
+
+#--- link the .gitconfig file
+cmd = "cd ~; ln -sf ./setupFiles/gitconfig .gitconfig"
+print cmd
+os.system(cmd)
+
+#--- if a home/bin directory doesnt exist, create it
+binDir = os.path.join(os.environ['HOME'],'bin')
+if not os.path.isdir(binDir):
+    print 'mkdir %s' % binDir
+    os.makedirs(binDir)
+
+#--- link the git diff wrapper
+cmd = "cd ~/bin; ln -s ~/setupFiles/git_diff_wrapper ."
+print cmd
+os.system(cmd)
+
