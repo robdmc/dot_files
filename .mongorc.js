05c100eb8dfebaafab9f84be73348e762d780649
Rob deCarvalho <robdmc@gmail.com>
2012-09-14 12:26:13 -0700

    Spruced up the mongorc script.

--- .mongorc.js
+++ .mongorc.js
@@ -4,8 +4,20 @@ prompt = function(){
   return db + "@" + HOST + " -->";
 }
 
-f = function(){};
-g = function(){};
-h = function(){};
-print('\nf(), g(), h() are three blank functions ready for editing');
-print('\nExample: Type "edit f" to open f() definition in vim\n');
+//f = function(){};
+//g = function(){};
+//h = function(){};
+//print('\nf(), g(), h() are three blank functions ready for editing');
+//print('\nExample: Type "edit f" to open f() definition in vim\n');
+print('\n\nAvailable databases\n-----------------');
+_INITFUNC = function(){
+  conn = new Mongo();
+  var dbList = conn.getDBNames();
+  for(var nr=0; nr<dbList.length; ++nr){
+    print(dbList[nr]);
+  }
+  print("\n");
+}
+_INITFUNC();
+
+//show dbs
