ce5f1a6e87169d053ecbeedf94461bbdb5d852ae
Rob deCarvalho <robdmc@gmail.com>
2014-06-24 12:38:59 -0400

    Added .ackrc to setup files.  Slight modification to condaSetup.sh

--- condaSetup.sh
+++ condaSetup.sh
@@ -1,3 +1,7 @@
 #export PATH="/Users/rob/anaconda/bin:$PATH"
 export PATH="$HOME/anaconda/bin:$PATH"
-export DYLD_FALLBACK_LIBRARY_PATH="$HOME/anaconda/lib:$DYLD_FALLBACK_LIBRARY_PATH"
+#export DYLD_FALLBACK_LIBRARY_PATH="$HOME/anaconda/lib:$DYLD_FALLBACK_LIBRARY_PATH"
+#export DYLD_LIBRARY_PATH="$HOME/anaconda/lib:usr/lib:/opt/local/lib"
+export LD_LIBRARY_PATH="$HOME/anaconda/lib:usr/lib:/opt/local/lib"
+
+
