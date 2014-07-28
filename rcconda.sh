6809e28cd938b2b3ce614ceafff92d170076ea2e
Rob deCarvalho <robdmc@gmail.com>
2014-07-28 17:28:23 -0400

    Changed the ldlibrary path in an attempt to get celery to work

--- rcconda.sh
+++ rcconda.sh
@@ -2,4 +2,4 @@
 export PATH="$HOME/anaconda/bin:$PATH"
 export DYLD_FALLBACK_LIBRARY_PATH="$HOME/anaconda/lib:$DYLD_FALLBACK_LIBRARY_PATH"
 #export DYLD_FALLBACK_LIBRARY_PATH="$HOME/usr/local/lib:$DYLD_FALLBACK_LIBRARY_PATH"
-export LD_LIBRARY_PATH="/usr/local/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="/usr/local/lib:/usr/lib:$LD_LIBRARY_PATH"
