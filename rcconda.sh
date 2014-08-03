534c0b0562b0cdebeee0dee5f1a29f68c353b6a6
Rob deCarvalho <robdmc@gmail.com>
2014-08-03 10:03:28 -0400

    cleaned up rcconda a bit

--- rcconda.sh
+++ rcconda.sh
@@ -1,5 +1,3 @@
-#export PATH="/Users/rob/anaconda/bin:$PATH"
 export PATH="$HOME/anaconda/bin:$PATH"
 export DYLD_FALLBACK_LIBRARY_PATH="$HOME/anaconda/lib:$DYLD_FALLBACK_LIBRARY_PATH"
-#export DYLD_FALLBACK_LIBRARY_PATH="$HOME/usr/local/lib:$DYLD_FALLBACK_LIBRARY_PATH"
 export LD_LIBRARY_PATH="/usr/local/lib:/usr/lib:$LD_LIBRARY_PATH"
