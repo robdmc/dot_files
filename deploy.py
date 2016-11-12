551e4563ecc9d405e59b5b2f88238b9b777cd2a7
Rob deCarvalho <robdmc@gmail.com>
2016-11-12 18:30:07 -0500

    makde deploy script python3 friendly

--- deploy.py
+++ deploy.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 
+from __future__ import print_function
 import os
 
 # --- create a list of files that should just be directly linked to home
@@ -21,68 +22,68 @@ linkFileList = [
 # --- link the files to home
 for linkFile in linkFileList:
     cmd = "cd ~; ln -sf ./dot_files/%s ." % linkFile
-    print cmd
+    print(cmd)
     os.system(cmd)
 
 # --- link the .gitconfig file
 cmd = "cd ~; ln -sf ./dot_files/gitconfig .gitconfig"
-print cmd
+print(cmd)
 os.system(cmd)
 
 # --- link the .gitignore file
 cmd = "cd ~; ln -sf ./dot_files/gitignore_global .gitignore_global"
-print cmd
+print(cmd)
 os.system(cmd)
 
 
 # --- if a home/bin directory doesnt exist, create it
 binDir = os.path.join(os.environ['HOME'],'bin')
 if not os.path.isdir(binDir):
-    print 'mkdir %s' % binDir
+    print('mkdir %s' % binDir)
     os.makedirs(binDir)
 
 # --- make an undo dir
 undo_dir = os.path.join(os.environ['HOME'],'.undodir')
 if not os.path.isdir(undo_dir):
-    print 'mkdir %s' % undo_dir
+    print('mkdir %s' % undo_dir)
     os.makedirs(undo_dir)
 
 # --- link executables
 cmd = "cd ~/bin; ln -sf ~/dot_files/git_diff_wrapper ."
-print cmd
+print(cmd)
 os.system(cmd)
 
 cmd = "cd ~/bin; ln -sf ~/dot_files/git_branch_diff ."
-print cmd
+print(cmd)
 os.system(cmd)
 
 cmd = "cd ~/bin; ln -sf ~/dot_files/circle_checker ."
-print cmd
+print(cmd)
 os.system(cmd)
 
 cmd = "cd ~/bin; ln -sf ~/dot_files/serve_dir.py ."
-print cmd
+print(cmd)
 os.system(cmd)
 
 cmd = "cd ~/bin; ln -sf ~/dot_files/imgcat ."
-print cmd
+print(cmd)
 os.system(cmd)
 
 
 # --- if a home/.config directory doesnt exist, create it
 binDir = os.path.join(os.environ['HOME'],'.config')
 if not os.path.isdir(binDir):
-    print 'mkdir %s' % binDir
+    print('mkdir %s' % binDir)
     os.makedirs(binDir)
 
 # --- link the git flake8 config file
 cmd = "cd ~/.config; ln -sf ~/dot_files/flake8 ."
-print cmd
+print(cmd)
 os.system(cmd)
 
 # --- create a bash_history file if it doesn't exist
 if not os.path.isfile(os.path.join(os.environ['HOME'], '.bash_history')):
     cmd = 'cp fake_bash_history {}/.bash_history'.format(os.environ['HOME'])
-    print cmd
+    print(cmd)
     os.system(cmd)
 
