d72dd1cb6cd837dcf7a3e9672be17021d7dec381
Rob deCarvalho <robdmc@gmail.com>
2017-01-24 20:47:38 -0500

    updated ngrok deploy

--- deploy_ngrok.py
+++ deploy_ngrok.py
@@ -8,11 +8,15 @@ import sys
 
 cmd = "p.crypt -i ngrok_config.tar.crypt -o ngrok_config.tar -d"
 print(cmd)
-os.system(cmd)
+if os.system(cmd):
+    print('failed', sys.stderr)
+    sys.exit()
 
 cmd = "cd ~ && tar -xvf ./dot_files/ngrok_config.tar"
 print(cmd)
-os.system(cmd)
+if os.system(cmd):
+    print('failed', sys.stderr)
+    sys.exit()
 
 
 platform_info = platform.platform().lower()
@@ -21,15 +25,19 @@ if 'darwin' in platform_info:
     cmd += ' && wget \'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip\''
     cmd += ' && unzip ngrok-stable-darwin-amd64.zip'
     cmd += ' && rm ngrok-stable-darwin-amd64.zip'
-    os.system(cmd)
+    if os.system(cmd):
+        print('failed', sys.stderr)
+        sys.exit()
 
 elif 'linux' in platform_info:
     cmd = 'cd ~/bin '
     cmd += ' && wget \'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip\''
     cmd += ' && unzip ngrok-stable-linux-amd64.zip'
     cmd += ' && rm ngrok-stable-linux-amd64.zip'
-    os.system(cmd)
+    if os.system(cmd):
+        print('failed', sys.stderr)
+        sys.exit()
 
 else:
-    print('\n\nArchitechure not recognized')
+    print('\n\nArchitechure not recognized', sys.stderr)
 
