204b89336af93ecfb37116b713ed4e1c75b4d0f3
Rob deCarvalho <robdmc@gmail.com>
2018-05-26 19:45:24 -0400

    updating conda install

--- conda_create_env.sh
+++ conda_create_env.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/env bash
-. ~/rcconda.sh
+mkdir -p ~/bash_hooks && \
+. ~/rcconda.sh && \
 conda env create --force -f ~/dot_files/environment.yml &&\
+cp ~/dot_files/viz_init.sh ~/bash_hooks && \
 ~/dot_files/conda_condigure_holoviews.sh
