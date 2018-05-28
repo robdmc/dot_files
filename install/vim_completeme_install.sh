71b29967266e4b27fabf53657ffc5ba728c0b13c
Rob deCarvalho <robdmc@gmail.com>
2018-05-28 13:26:24 -0400

    wip

--- install/vim_completeme_install.sh
+++ install/vim_completeme_install.sh
@@ -3,9 +3,10 @@
 rm -rf ~/dot_files/.vim/bundle/YouCompleteMe 2>/dev/null || true
 cd ~/dot_files/.vim/bundle && \
 git clone https://github.com/Valloric/YouCompleteMe.git && \
-cd YouCompleteMe
+cd YouCompleteMe && \
 git submodule update --init --recursive && \
 python install.py
+
 
 
 
