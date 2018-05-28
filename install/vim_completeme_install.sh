894972f6a1adb145518acaec0c2b2361e22cfa46
Rob deCarvalho <robdmc@gmail.com>
2018-05-28 14:46:00 -0400

    fixed stupid bug in vim youcompleteme

--- install/vim_completeme_install.sh
+++ install/vim_completeme_install.sh
@@ -1,8 +1,8 @@
 #! /usr/bin/env bash
 
-brew install cmake && \
-rm -rf ~/dot_files/.vim/bundle/YouCompleteMe 2>/dev/null || true
-cd ~/dot_files/.vim/bundle && \
+brew install cmake || true
+rm -rf ~/.vim/bundle/YouCompleteMe 2>/dev/null || true
+cd ~/.vim/bundle && \
 git clone https://github.com/Valloric/YouCompleteMe.git && \
 cd YouCompleteMe && \
 git submodule update --init --recursive && \
