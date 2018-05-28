d22c8e46a42eb64a1a72a7153e8819067d6666c9
Rob deCarvalho <robdmc@gmail.com>
2018-05-28 14:33:02 -0400

    added cmake install to vim install stuff

--- install/vim_completeme_install.sh
+++ install/vim_completeme_install.sh
@@ -1,5 +1,6 @@
 #! /usr/bin/env bash
 
+brew install cmake && \
 rm -rf ~/dot_files/.vim/bundle/YouCompleteMe 2>/dev/null || true
 cd ~/dot_files/.vim/bundle && \
 git clone https://github.com/Valloric/YouCompleteMe.git && \
