e1eef781050f00e2545f47b9738dc8dccb2ad58a
Rob deCarvalho <robdmc@gmail.com>
2018-06-14 10:19:25 -0400

    added ability to handle ambition gold repo

--- rc_bash_aliases.sh
+++ rc_bash_aliases.sh
@@ -11,12 +11,17 @@
      export LSCOLORS=GxFxCxDxBxegedabagaced
  fi
 
+ export GOLD_AMBITION_HOME="/Users/rob/rob/gold_repos/ambition" 
+ export GOLD_HISTORY_DIR="$GOLD_AMBITION_HOME/docker_bash_history"
  # Specific to my day job
  alias ga='cd /Users/rob/ambition'
  alias gp='cd /Users/rob/packages'
  alias gadp='lamb.docker shell -p'
  alias gad='lamb.docker shell'
- alias dad='(cp /Users/rob/ambition/docker_bash_history/.bash_history `date +"/Users/rob/docker_history_files/docker_bash_history_%FT%H-%M-%S"`);  lamb.docker down'
+ alias gadpg='(AMBITION_HOME="$GOLD_AMBITION_HOME" lamb.docker shell -p)'
+ alias gadg='(AMBITION_HOME="$GOLD_AMBITION_HOME" lamb.docker shell)'
+ alias dad='(cp /Users/rob/ambition/docker_bash_history/.bash_history `date +"/Users/rob/docker_history_files/docker_bash_history_%FT%H-%M-%S"`);  AMBITION_HOME="$GOLD_AMBITION_HOME" lamb.docker down'
+ alias dadg='(cp "$GOLD_HISTORY_DIR/.bash_history" `date +"$GOLD_HISTORY_DIR/docker_bash_history_%FT%H-%M-%S"`); AMBITION_HOME="$GOLD_AMBITION_HOME"  lamb.docker down'
  alias ldc='docker ps --format "{{.ID}} {{.Names}}"'
 
  # Specific to machines I frequent
