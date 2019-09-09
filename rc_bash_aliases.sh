c21eb4683276f786724bae740396454ddb9a92ff
Rob deCarvalho <robdmc@gmail.com>
2019-09-09 12:54:12 -0400

    wip

--- rc_bash_aliases.sh
+++ rc_bash_aliases.sh
@@ -22,7 +22,7 @@
  alias gadp='docker-compose -f /Users/rob/rob/ambition_projects/projects/ambition/docker-compose.yml run --rm  --service-ports shell'
  alias gadpg='(AMBITION_HOME="$GOLD_AMBITION_HOME" lamb.docker shell -p)'
  alias gadg='(AMBITION_HOME="$GOLD_AMBITION_HOME" lamb.docker shell)'
- alias dad='(cp /Users/rob/ambition/docker_bash_history/.bash_history `date +"/Users/rob/docker_history_files/docker_bash_history_%FT%H-%M-%S"`);  AMBITION_HOME="$GOLD_AMBITION_HOME" lamb.docker down'
+ alias dad='(cp /Users/rob/ambition/docker_bash_history/.bash_history `date +"/Users/rob/docker_history_files/docker_bash_history_%FT%H-%M-%S"`);  cd $AMBITION_HOME && docker-compose down'
  alias dadg='(cp "$GOLD_HISTORY_DIR/.bash_history" `date +"$GOLD_HISTORY_DIR/docker_bash_history_%FT%H-%M-%S"`); AMBITION_HOME="$GOLD_AMBITION_HOME"  lamb.docker down'
  alias ldc='docker ps --format "{{.ID}} {{.Names}}"'
 
@@ -36,7 +36,13 @@
  alias ipywi='BROWSER=open jupyter notebook --port=8888 --NotebookApp.iopub_data_rate_limit=10000000000'
  alias jup='BROWSER=open jupyter notebook --ip=0.0.0.0 --port=8888 --NotebookApp.iopub_data_rate_limit=10000000000'
  alias jupl='BROWSER=open jupyter notebook --port=8888 --NotebookApp.iopub_data_rate_limit=10000000000'
+ alias jl='BROWSER=open jupyter lab --NotebookApp.iopub_data_rate_limit=10000000000'
 
  # Alias for opening macvim
  alias mvim='/Applications/MacVim.app/Contents/MacOS/Vim -g'
+
+ # Alias for gcalcli
+ alias gc='gcalcli agenda --refresh'
+ alias gcw='gcalcli calw --refresh'
+ alias gcm='gcalcli calm --refresh'
 
