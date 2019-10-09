ecec50a0cf81dd99a64c02e9e62f9e583ed9f7e4
Rob deCarvalho <robdmc@gmail.com>
2019-10-09 08:16:18 -0400

    wip

--- rc_bash_aliases.sh
+++ rc_bash_aliases.sh
@@ -22,7 +22,8 @@
  alias gadp='docker-compose -f /Users/rob/rob/ambition_projects/projects/ambition/docker-compose.yml run --rm  --service-ports shell'
  alias gadpg='(AMBITION_HOME="$GOLD_AMBITION_HOME" lamb.docker shell -p)'
  alias gadg='(AMBITION_HOME="$GOLD_AMBITION_HOME" lamb.docker shell)'
- alias dad='(cp /Users/rob/ambition/docker_bash_history/.bash_history `date +"/Users/rob/docker_history_files/docker_bash_history_%FT%H-%M-%S"`);  cd $AMBITION_HOME && docker-compose down'
+ # alias dad='(cp /Users/rob/ambition/docker_bash_history/.bash_history `date +"/Users/rob/docker_history_files/docker_bash_history_%FT%H-%M-%S"`);  cd $AMBITION_HOME && docker-compose down'
+ alias dad='(cp /Users/rob/ambition/.lamb/config/container_bash_history `date +"/Users/rob/docker_history_files/docker_bash_history_%FT%H-%M-%S"`);  cd $AMBITION_HOME && docker-compose down'
  alias dadg='(cp "$GOLD_HISTORY_DIR/.bash_history" `date +"$GOLD_HISTORY_DIR/docker_bash_history_%FT%H-%M-%S"`); AMBITION_HOME="$GOLD_AMBITION_HOME"  lamb.docker down'
  alias ldc='docker ps --format "{{.ID}} {{.Names}}"'
 
