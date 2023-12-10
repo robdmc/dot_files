f6ba38904d413d6d737bfa5718a051d76425eac6
Robert deCarvalho <robdmc@gmail.com>
2023-12-10 12:43:11 -0500

    wip

---
+++ logseq_dirs.sh
@@ -0,0 +1,10 @@
+DIRS=(
+"$HOME/rob/repos/blueberry_logseq/"
+"$HOME/rob/repos/references_logseq/"
+"$HOME/rob/repos/ambition_logseq"
+"$HOME/rob/repos/renovation_logseq"
+"$HOME/rob/repos/personal_logseq"
+# "$HOME/rob/repos/code_logseq"
+"$HOME/rob/repos/consulting_logseq"
+)
+
