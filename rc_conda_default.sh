176cae3155845057205c725102cac4500571704a
Rob deCarvalho <robdmc@gmail.com>
2024-03-22 10:34:43 -0400

    wip

---
+++ rc_conda_default.sh
@@ -0,0 +1,9 @@
+ # Requires OS_TYPE 
+
+ # If on mac, update paths with mac-specific locations
+ if [ "$OS_TYPE" == "mac" ]; then 
+     export CONDA_ENV_DEFAULT=""
+ else
+     export CONDA_ENV_DEFAULT=""
+ fi
+
