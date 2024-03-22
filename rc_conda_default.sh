f11b54f513487b95f312eed7d1f8065be9b088e6
Rob deCarvalho <robdmc@gmail.com>
2024-03-22 10:50:01 -0400

    wip

--- rc_conda_default.sh
+++ rc_conda_default.sh
@@ -1,5 +1,6 @@
  # Requires OS_TYPE 
 
  # export CONDA_ENV_DEFAULT="ai_2024_02_29"
- export CONDA_ENV_DEFAULT="ai_2024_03_18"
+ # export CONDA_ENV_DEFAULT="ai_2024_03_18"
+ export CONDA_ENV_DEFAULT="change_this_to_your_desired_conda_env_in_dot_files_rc_conda_default"
 
