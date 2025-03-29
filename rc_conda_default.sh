05d9f1876c1d3c11f7387dfdf78b358ea43140b9
Generic User <generic@user.net>
2025-03-29 12:39:19 -0400

    auto

--- rc_conda_default.sh
+++ rc_conda_default.sh
@@ -2,5 +2,5 @@
 
  # export CONDA_ENV_DEFAULT="ai_2024_02_29"
  # export CONDA_ENV_DEFAULT="ai_2024_03_18"
- export CONDA_ENV_DEFAULT="change_this_to_your_desired_conda_env_in_dot_files_rc_conda_default"
+ export CONDA_ENV_DEFAULT="ai_2024_06_16"
 
