848bb45f7799ce02e4f8e47110efb7aba3fad691
Rob deCarvalho <robdmc@gmail.com>
2025-03-31 10:34:03 -0400

    wip

--- rc_conda_default.sh
+++ rc_conda_default.sh
@@ -1,7 +1,22 @@
  # Requires OS_TYPE 
 
- # export CONDA_ENV_DEFAULT="ai_2024_02_29"
- # export CONDA_ENV_DEFAULT="ai_2024_03_18"
- export CONDA_ENV_DEFAULT="ai_2024_06_16"
  # export CONDA_ENV_DEFAULT="change_this_to_your_desired_conda_env_in_dot_files_rc_conda_default"
+ # export CONDA_ENV_DEFAULT="ai_2024_02_29"
+ #
+ # export CONDA_ENV_DEFAULT="ai_2024_12_10"
+  export CONDA_ENV_DEFAULT="ai_2024_07_28"  # This is good for legacy forecasting and payroll
+ # export CONDA_ENV_DEFAULT="ai_2024_06_17"
+ # export CONDA_ENV_DEFAULT="ai_2024_06_16"
+ # export CONDA_ENV_DEFAULT="ai_2024_03_18"
+ # export CONDA_ENV_DEFAULT="ai_django_2024_04_25"
+ # export CONDA_ENV_DEFAULT="ai_django_simple_2024_04_25"
+
+
+  # I am hijacking this conda file to also specify uv environments.
+  # export UV_ENV_DEFAULT="/Users/rob/rob/repos/uv_management/ai_2025-01-27"
+  #export UV_ENV_DEFAULT="/Users/rob/rob/repos/uv_management/ai_2025-01-28"
+  export UV_ENV_DEFAULT="/Users/rob/rob/repos/uv_management/ai_2025-02-26"
+
+
+  # IGNORE ME
 
